5ff245909fab627bce962fc3c635d5002ba9232b
Ivo de Ruever <ivoderuever@gmail.com>
2021-05-20 09:41:59 +0200

    Add color generation code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="number" placeholder="amount" v-model="amount" />
+    <button @click="generateFile()">Generate</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data: () => {
+    return {
+      amount: 0,
+      colors: []
+    };
+  },
+  methods: {
+    randomColor() {
+      return "#000000".replace(/0/g, function () {
+        return (~~(Math.random() * 16)).toString(16);
+      });
+    },
+    generateFile() {
+      //generated given amount of color codes
+      for(let i = 0; i < this.amount; i++ ) {
+        this.colors.push(this.randomColor());
+      }
+      console.log(this.colors);
+
+      //creates file and enables dowload of set file
+      const data = JSON.stringify({colors: this.colors})
+      const blob = new Blob([data], {type: 'text/plain'})
+      const e = document.createEvent('MouseEvents'),
+      a = document.createElement('a');
+      a.download = "colors.json";
+      a.href = window.URL.createObjectURL(blob);
+      a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+      e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+      a.dispatchEvent(e);
+
+      //clear colors array
+      this.colors = []
+    },
+  },
+};
 </script>
+
+<style lang="scss">
+</style>
