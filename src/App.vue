9a8460bd3e345c42b96cfe7aca7cc92a107d89d6
ivoderuever <ivoderuever@gmail.com>
2021-05-24 21:29:50 +0200

    Add a coat of paint

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div class="center">
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import url("https://use.typekit.net/eqp3quc.css");
+
+html,
+body {
+  background-color: #0c0c0c;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: sofia-pro;
+
+  @media not all and (hover: hover) {
+    height: 100%;
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="home">
-    <input type="number" placeholder="amount" v-model="amount" />
-    <button @click="generateFile()">Generate</button>
+    <h2>Enter the amount of color codes you want in the json file<span class="primary">.</span></h2>
+    <div class="input">
+      <input type="number" placeholder="Amount of color codes" v-model="amount" />
+      <button @click="generateFile()">
+        <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.75 14.75V16.25C4.75 17.9069 6.09315 19.25 7.75 19.25H16.25C17.9069 19.25 19.25 17.9069 19.25 16.25V14.75"></path>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 14.25L12 4.75"></path>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M8.75 10.75L12 14.25L15.25 10.75"></path>
+        </svg>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -12,7 +21,7 @@ export default {
   name: "Home",
   data: () => {
     return {
-      amount: 0,
+      amount: null,
       colors: []
     };
   },
@@ -24,28 +33,72 @@ export default {
     },
     generateFile() {
       //generated given amount of color codes
-      for(let i = 0; i < this.amount; i++ ) {
-        this.colors.push(this.randomColor());
+      if(this.amount > 0) {
+        for(let i = 0; i < this.amount; i++ ) {
+          this.colors.push(this.randomColor());
+        }
+        console.log(this.colors);
+  
+        //creates file and enables dowload of set file
+        const data = JSON.stringify({colors: this.colors})
+        const blob = new Blob([data], {type: 'text/plain'})
+        const e = document.createEvent('MouseEvents'),
+        a = document.createElement('a');
+        a.download = "colors.json";
+        a.href = window.URL.createObjectURL(blob);
+        a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+        e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        a.dispatchEvent(e);
+  
+        //clear colors array
+        this.colors = []
+      } else {
+        alert('You need to give a number higher than 0')
       }
-      console.log(this.colors);
-
-      //creates file and enables dowload of set file
-      const data = JSON.stringify({colors: this.colors})
-      const blob = new Blob([data], {type: 'text/plain'})
-      const e = document.createEvent('MouseEvents'),
-      a = document.createElement('a');
-      a.download = "colors.json";
-      a.href = window.URL.createObjectURL(blob);
-      a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
-      e.initEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-      a.dispatchEvent(e);
-
-      //clear colors array
-      this.colors = []
     },
   },
 };
 </script>
 
 <style lang="scss">
+  .home {
+    position: relative;
+    display: inline-block;
+    h2 {
+      text-align: center;
+      color: #f1f1f1;
+    }
+    .primary {
+      color: #6037f3;
+    }
+
+    .input {
+      display: flex;
+      justify-content: center;
+      margin: 0 auto;
+
+      input {
+        border: 2px solid #6037f3;
+        padding: 7px 15px 10px 15px;
+        font-size: 16px;
+        border-radius: 20px 0px 0px 20px;
+        font-family: inherit;
+
+        &:focus {
+          outline: none;
+        }
+      }
+
+      button {
+        background-color: #6037f3;
+        border: none;
+        border-radius: 0px 20px 20px 0px;
+        padding: 10px 10px 7px 7px;
+        cursor: pointer;
+        font-size: 14px;
+        font-family: inherit;
+        color: #f1f1f1;
+      }
+    }
+  }
 </style>
